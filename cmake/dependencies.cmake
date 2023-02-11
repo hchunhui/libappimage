441850b1ab46e811832584e8db966b634786829f
Chunhui He <hchunhui@mail.ustc.edu.cn>
2023-02-11 06:37:45 +0000

    update zstd to 1.5.4

--- cmake/dependencies.cmake
+++ cmake/dependencies.cmake
@@ -57,8 +57,8 @@ if(NOT USE_SYSTEM_ZSTD)
     message(STATUS "Downloading and building zstd")
 
     ExternalProject_Add(zstd-EXTERNAL
-        URL https://github.com/facebook/zstd/releases/download/v1.5.0/zstd-1.5.0.tar.gz
-        URL_HASH SHA512=b322fc1b89a556827b7fece2fb0f34c83bf65bb85b2468c791d6d9178a65c81e21f4171b7533cbf12bc1dfb2fd323d3e8c34d86167b157645c27f65186eec659
+        URL https://github.com/facebook/zstd/releases/download/v1.5.4/zstd-1.5.4.tar.gz
+        URL_HASH SHA512=2896a6dd6b60cc251720356babcbab6018c874eb2149121b26e28041496fc355a9cb5fd1b39c91558fcfbafb789b3d721264a0f9b5734f893d5f3cdf97016394
         CONFIGURE_COMMAND echo configure zstd
         BUILD_COMMAND CC=${CC} CXX=${CXX} CFLAGS=-fPIC CPPFLAGS=${CPPFLAGS} LDFLAGS=${LDFLAGS} ${MAKE} -C<SOURCE_DIR>/lib ZSTD_LIB_MINIFY=1 libzstd.a
         INSTALL_COMMAND ${MAKE} -C<SOURCE_DIR>/lib PREFIX=<INSTALL_DIR> install-static install-includes
